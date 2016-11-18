0671f7e4d420df2dc389dcf5cca7febb862af977
Matthew Elmquist <matthewelmquist@Matthews-MacBook-Pro.local>
2016-11-18 17:48:02 -0500

    search for songs, integrated with store

---
+++ app/components/Search.jsx
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {RaisedButton, TextField, MuiThemeProvider} from 'material-ui';
+import {fetchTrackResults} from '../ducks/searchResults';
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
+
+/* -----------------    DUMB COMPONENT     ------------------ */
+
+const DumbSearch = props => {
+  const { onType, trackSearch } = props;
+  return (
+    <div>
+        <TextField onChange={ onType }
+             floatingLabelText="Search By Track"
+           />
+         <RaisedButton label="Search"  onTouchTap={ trackSearch }/>
+    </div>
+  );
+};
+
+
+/* -----------------    STATEFUL REACT COMPONENT     ------------------ */
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ''
+    }
+
+    this.onType = this.onType.bind(this);
+    this.trackSearch = this.trackSearch.bind(this);
+  }
+
+  onType(evt) {
+    evt.preventDefault();
+    let query = evt.target.value;
+    this.setState({ query });
+  }
+
+
+  trackSearch(evt){
+    const {tracksearch} = this.props
+    const {query} = this.state
+    tracksearch(query)
+  }
+
+  render() {
+
+    return (
+      <MuiThemeProvider>
+        <DumbSearch
+          onType={ this.onType }
+          trackSearch={ this.trackSearch }
+        />
+      </MuiThemeProvider>
+    );
+  }
+}
+
+
+/* -----------------    CONTAINER     ------------------ */
+
+// const mapStateToProps = () => ();
+const mapDispatchToProps = dispatch => ({
+  tracksearch: (query) => dispatch(fetchTrackResults(query))
+});
+
+export default connect(null, mapDispatchToProps)(Search);
--- app/routes.js
+++ app/routes.js
@@ -3,12 +3,14 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Main from './components/Main'
 import Chat from './components/Chat'
+import Search from './components/Search'
 
 import { loadFirebase } from './onEnterHooks'
 
 export default () => (
   <Router history={browserHistory}>
     <Route path="/" component={Main} onEnter={loadFirebase}>
+      <Route path="/search" component={Search} />
       <Route path="/chat" component={Chat} />
     </Route>
   </Router>

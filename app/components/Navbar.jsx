85dfd3b21c47bb2440662f9e624bed6c461e4f45
Matthew Elmquist <mattelmquist@gmail.com>
2016-11-20 12:22:07 -0500

    navbar location copied from dillons login branch

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,2 +1,16 @@
-//testing to see if my commits are working on Github...
-//another testing line
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import Player from './Player';
+
+
+/* -----------------    COMPONENT     ------------------ */
+
+export default props => (
+  <div>
+    <Link to="/app/search">Search</Link>
+    <Link to="/app/chat">Chat</Link>
+    <Link to="/app/songs">Songs</Link>
+    { props.children }
+    <Player/>
+  </div>
+);

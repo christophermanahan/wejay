1a3a0603a66141a487cf95a435bcbdef72ea89fb
Shane Jarman <sjarman91@gmail.com>
2016-11-23 15:18:32 -0500

    really working

--- app/components/HostPlayer.jsx
+++ app/components/HostPlayer.jsx
@@ -4,7 +4,7 @@ import LinearProgress from 'material-ui/LinearProgress';
 import IconButton from 'material-ui/IconButton';
 import PlayCircleOutline from 'material-ui/svg-icons/av/play-circle-outline';
 import PauseCircleOutline from 'material-ui/svg-icons/av/pause-circle-outline';
-import publicKeys from './utils/publicKeys'
+import publicKeys from '../utils/publicKeys'
 
 import { connect } from 'react-redux';
 

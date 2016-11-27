c2db82107021a685fa0e2b4b3c2c5a15720a85ed
Shane Jarman <sjarman91@gmail.com>
2016-11-26 19:55:58 -0500

    update dj name functional

--- app/components/Djs.jsx
+++ app/components/Djs.jsx
@@ -1,32 +1,72 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
+import { TextField } from 'material-ui';
 import DjList from './DjsList'
 
 /* -----------------    DUMB COMPONENT     ------------------ */
 
-const DjsComponent = props => {
-  const { djs, user } = props;
-  const djPoints = djs[user.uid] && djs[user.uid].dj_points;
-  delete djs[user.uid];
-  return (
-    <div>
-      <h1>Your DJ Data</h1>
-      <p>DJ {user.displayName}</p>
-      <p>Points: {djPoints}</p>
-      <hr/>
-      <h1> Party DJs</h1>
-      <DjList djs={djs}/>
-    </div>
-  );
-};
-
 /* -----------------    STATEFUL REACT COMPONENT     ------------------ */
 
+class DjsComponent extends Component {
+  constructor(props) {
+    super(props);
+    const { djs, user } = this.props;
+    let djName = djs[user.uid] && djs[user.uid].dj_name;
+
+    this.state = { value: djName }
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { djs, user } = nextProps;
+    let djName = djs[user.uid] && djs[user.uid].dj_name;
+    this.setState({value: djName});
+  }
+
+  onSubmit(evt) {
+    evt.preventDefault();
+    const { firebase, currentParty, user } = this.props
+    firebase.database().ref('party_djs').child(currentParty.id).child(user.uid)
+    .update({dj_name: this.state.value})
+  }
+
+  handleChange(evt) {
+    evt.preventDefault();
+    this.setState({value: evt.target.value})
+  }
+
+
+  render() {
+    const { djs, user } = this.props;
+    const djPoints = djs[user.uid] && djs[user.uid].dj_points;
+    let djName = djs[user.uid] && djs[user.uid].dj_name;
+    const otherDjs = Object.assign({}, djs)
+    delete otherDjs[user.uid];
+
+    return (
+        <div>
+          <h1>Your DJ Data</h1>
+          <p>{user.displayName}</p>
+          <form onSubmit={this.onSubmit}>
+            <label> DJ NAME --- </label>
+            <input value={this.state.value || ''} onChange={this.handleChange}></input>
+            <button type="submit">update my dj name</button>
+          </form>
+          <br />
+          <p>Points: {djPoints}</p>
+          <hr/>
+          <h1> Party DJs</h1>
+          <DjList djs={otherDjs}/>
+        </div>
+    );
+  }
+}
 
 /* -----------------    CONTAINER     ------------------ */
 
-const mapStateToProps = ({ firebase, user, djs }) => ({ firebase, user, djs });
+const mapStateToProps = ({ firebase, user, djs, currentParty }) => ({ firebase, user, djs, currentParty });
 
 export default connect(mapStateToProps)(DjsComponent);
 

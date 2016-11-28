7fd9015027422b1295b48bf1175a72ff18470b63
Shane Jarman <sjarman91@gmail.com>
2016-11-28 14:27:25 -0500

    dj styles updated

--- app/components/DjsList.jsx
+++ app/components/DjsList.jsx
@@ -6,15 +6,15 @@ import { Row, Col } from 'react-flexbox-grid/lib/index';
 const SingleDj = props => {
   const { user, dj } = props;
   return (
-    <Col xs={12}>
-      <Row style={{border:"1px solid black"}}>
-        <Col xs={3}>
-        <img height="150" width="150" src="https://thumbnailer.mixcloud.com/unsafe/318x318/extaudio/0/9/b/f/ce50-0b29-40a3-a31b-95b1b9b38c5c" />
+    <Col className="single-dj" xs={12}>
+      <Row>
+        <Col xsOffset={1} xs={4}>
+          <img height="75" width="75" src="https://thumbnailer.mixcloud.com/unsafe/318x318/extaudio/0/9/b/f/ce50-0b29-40a3-a31b-95b1b9b38c5c" />
         </Col>
-        <Col xs={9}>
-        <p>Name: {dj.dj_name}</p>
-        <p>Points: {dj.dj_points}</p>
-        {dj.uid === user.uid ? <p>This is you!</p> : <div></div>}
+        <Col xs={6}>
+          <p>{dj.dj_name}</p>
+          <p>Points: {dj.dj_points}</p>
+          {dj.uid === user.uid ? <p>This is you!</p> : <div></div>}
         </Col>
       </Row>
     </Col>

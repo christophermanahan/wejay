e282ee1a51aa726143c562b7570414b3459d1b20
Shane Jarman <sjarman91@gmail.com>
2016-11-28 18:06:30 -0500

    updated package.json

--- app/utils/publicKeys.jsx
+++ app/utils/publicKeys.jsx
@@ -1,4 +1,13 @@
-const publicKeys = {
+const livePublicKeys = {
+  FIREBASE_API_KEY: "AIzaSyBIFrvUppZxiLTdrjLxtKGEcNNwMt0S3bM",
+  FIREBASE_AUTH_DOMAIN: "wejay-live.firebaseapp.com",
+  FIREBASE_DATABASE_URL: "https://wejay-live.firebaseio.com",
+  FIREBASE_STORAGE_BUCKET: "wejay-live.appspot.com",
+  FIREBASE_MESSAGING_SENDER_ID: "486696039321",
+  SC_CLIENT_ID: "033e31e2d036e02f39242e1aa1dd2fa9"
+}
+
+const productionKeys = {
   FIREBASE_API_KEY: "AIzaSyCiLc4HnRXwvVzeAoBQTOjiID2i8Drd-IM",
   FIREBASE_AUTH_DOMAIN: "wejay-ac08c.firebaseapp.com",
   FIREBASE_DATABASE_URL: "https://wejay-ac08c.firebaseio.com",
@@ -7,4 +16,4 @@ const publicKeys = {
   SC_CLIENT_ID: "033e31e2d036e02f39242e1aa1dd2fa9"
 }
 
-export default publicKeys
+const publicKeys = livePublicKeys

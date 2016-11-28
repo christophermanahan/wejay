c0c90e842c910d423cc56dc9ba90ccc479068870
Christopher Manahan <chris.a.manahan@gmail.com>
2016-11-28 17:25:29 -0500

    created my songs component view

--- app/components/MySongs.jsx
+++ app/components/MySongs.jsx
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import Audiotrack from 'material-ui/svg-icons/image/audiotrack';
 
-/* -----------------    DUMB COMPONENT     ------------------ */
+/* -----------------    DUMB COMPONENTS     ------------------ */
+
+const DumbSong = props => {
+  const { title, artist, artwork_url } = props;
+  console.log("TITLTLELELE", title)
+  return (
+    <ListItem
+      primaryText={title}
+      secondaryText={artist}
+      leftAvatar={artwork_url ? <Avatar src={artwork_url}/> : <Avatar color={cyan500} backgroundColor='#363836' icon={<Audiotrack />}/>}
+    />
+  )
+}
 
 const DumbMySongs = props => {
+  const { personalQueue } = props;
+  console.log('PQPQPQPQPQ', personalQueue)
+  let songArr = [];
+  for (let song in personalQueue) { songArr.push(personalQueue[song]) };
+  console.log('SONG ARRAY', songArr)
   return (
     <div>
-      <h2>I am the MySongs Component!</h2>
+      <List>
+        { songArr.length && songArr.map((song, i) => {
+          console.log("THIS IS A SONG", song)
+          return 
+            <DumbSong
+              title={song.title}
+              key={i}
+              artwork_url={song.artwork_url}
+              artist={song.artist}
+            />
+        })}
+      </List>
     </div>
   );
 };
@@ -20,11 +51,16 @@ class MySongs extends Component {
     super(props);
   }
 
-  render() {
+  componentDidMount() {
+    let { personalQueue } = this.props
+    console.log('HERE', personalQueue)
+  }
 
+  render() {
+    let { personalQueue } = this.props;
+    console.log('rendering', personalQueue)
     return (
-      <DumbMySongs
-      />
+      <DumbMySongs personalQueue={personalQueue}/>
     );
   }
 }
@@ -32,7 +68,7 @@ class MySongs extends Component {
 
 /* -----------------    CONTAINER     ------------------ */
 
-const mapStateToProps = ({  }) => ({  });
+const mapStateToProps = ({ personalQueue }) => ({ personalQueue });
 
 
 export default connect(mapStateToProps)(MySongs);

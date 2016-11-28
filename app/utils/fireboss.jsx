00a6832c33405aa6666e53fc27af258c3f2b93b6
Shane Jarman <sjarman91@gmail.com>
2016-11-28 01:01:47 -0500

    sorted djs

--- app/utils/fireboss.jsx
+++ app/utils/fireboss.jsx
@@ -95,10 +95,19 @@ Fireboss.prototype.addToPersonalQueue = function(partyId, user, song) {
 
 Fireboss.prototype.incrementVotePriority = function(partyId, songId) {
   const partyTopTenSongRef = this.database.ref('top_ten').child(partyId).child(songId)
-  // get snapshot of song, then add 1 to vote priority
+  // get snapshot of song, then add 1 to vote priority and djPoints
   partyTopTenSongRef.once('value')
     .then(snapshot => {
       const currentVotes = snapshot && snapshot.val().vote_priority
+      const userId = snapshot && snapshot.val().uid
+
+      this.database.ref('party_djs').child(partyId).child(userId).once('value')
+        .then(snapshot => {
+          const currentDjPoints = snapshot && snapshot.val().dj_points
+          this.database.ref('party_djs').child(partyId).child(userId)
+            .update({dj_points: (currentDjPoints + 1)})
+        })
+
       return partyTopTenSongRef.update({vote_priority: (currentVotes + 1)})
     })
     .then(() => {console.log('vote added!')})
@@ -110,6 +119,15 @@ Fireboss.prototype.decrementVotePriority = function(partyId, songId) {
   partyTopTenSongRef.once('value')
     .then(snapshot => {
       const currentVotes = snapshot && snapshot.val().vote_priority
+      const userId = snapshot && snapshot.val().uid
+
+      this.database.ref('party_djs').child(partyId).child(userId).once('value')
+        .then(snapshot => {
+          const currentDjPoints = snapshot && snapshot.val().dj_points
+          this.database.ref('party_djs').child(partyId).child(userId)
+            .update({dj_points: (currentDjPoints - 1)})
+        })
+
       return partyTopTenSongRef.update({vote_priority: (currentVotes - 1)})
     })
     .then(() => {console.log('vote added!')})

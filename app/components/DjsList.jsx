516642f6317f1af0d907e48657b00e7ae909eee6
Shane Jarman <sjarman91@gmail.com>
2016-11-28 16:15:12 -0500

    dj user profiles loading

--- app/components/DjsList.jsx
+++ app/components/DjsList.jsx
@@ -5,19 +5,36 @@ import { Row, Col } from 'react-flexbox-grid/lib/index';
 
 const SingleDj = props => {
   const { user, dj } = props;
+
   return (
     <Col className="single-dj" xs={12}>
-      <Row>
-        <Col xsOffset={1} xs={4}>
-          <img height="75" width="75" src="https://thumbnailer.mixcloud.com/unsafe/318x318/extaudio/0/9/b/f/ce50-0b29-40a3-a31b-95b1b9b38c5c" />
-        </Col>
-        <Col xs={6}>
-          <p>{dj.dj_name}</p>
-          <p>Points: {dj.dj_points}</p>
-          {dj.uid === user.uid ? <p>This is you!</p> : <div></div>}
-        </Col>
-      </Row>
+      {dj.uid === user.uid ?
+            <Row >
+              <Col xsOffset={1} xs={4}>
+                <div>
+                  <img className="dj-img" src={dj.photo} />
+                </div>
+              </Col>
+              <Col xs={6}>
+                <p>{dj.dj_name}</p>
+                <p>Points: {dj.dj_points}</p>
+              </Col>
+            </Row>
+        :
+            <Row>
+              <Col xsOffset={1} xs={4}>
+                <div>
+                  <img className="dj-img" src={dj.photo} />
+                </div>
+              </Col>
+              <Col xs={6}>
+                <p><strong>You</strong></p>
+                <p><strong>Points: {dj.dj_points}</strong></p>
+              </Col>
+            </Row>
+      }
     </Col>
+
   );
 };
 
@@ -26,7 +43,7 @@ const DjsList = props => {
   const { djs, user } = props
   return (
     <Row>
-      {djs && djs.map(dj => <SingleDj user={user} dj={dj} />)}
+      {djs && djs.map(dj => <SingleDj key={dj.uid} user={user} dj={dj} />)}
     </Row>
   );
 };

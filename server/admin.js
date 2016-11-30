f2f44d9e671cf4b2edfbed0ae72359798cbc0ca3
Dillon Powers <dillon.c.powers@gmail.com>
2016-11-30 12:59:08 -0500

    fix bug in next btn

--- server/admin.js
+++ server/admin.js
@@ -10,6 +10,7 @@ const auth_uri = process.env.FIREBASE_ADMIN_AUTH_URI;
 const token_uri = process.env.FIREBASE_ADMIN_TOKEN_URI;
 const auth_provider_x509_cert_url = process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL;
 const client_x509_cert_url = process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL;
+const databaseURL = process.env.FIREBASE_DATABASE_URL;
 // testing for heroku
 const serviceAccount = {
 	type,
@@ -26,7 +27,7 @@ const serviceAccount = {
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://wejay-live.firebaseio.com'
+	databaseURL
 });
 const db = admin.database();
 

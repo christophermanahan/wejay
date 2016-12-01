21ab553e2667dc435b29781603c7d97e89383284
Dillon Powers <dillon.c.powers@gmail.com>
2016-12-01 12:19:41 -0500

    fs cleanup

---
+++ tests/components/Djs.spec.jsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import { expect } from 'chai';
+// import { shallow } from 'enzyme';
+// import { spy } from 'sinon';
+
+// import { Djs } from '../../app/components/Djs';
+
+// console.log('Djs is', Search)
+
+// describe('<Djs />', () => {
+
+// 	let onTypeSpy, trackSearchSpy, search;
+
+// 	beforeEach('Create shallow copy with two spy fns on props', () => {
+// 		onTypeSpy = spy();
+// 		trackSearchSpy = spy();
+// 		search = shallow(<Djs />);
+// 	});
+
+// 	it('should do some stuff', () => {
+// 		console.log('ran test');
+// 	});
+
+// 	it('should do more stuff', () => {
+// 		console.log('ran test')
+// 	});
+
+
+// })
+
+// // onType={ onTypeSpy } trackSearch={ trackSearchSpy }

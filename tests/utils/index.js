9f9150ff5a385faf090019e32b01ffb00e90bd71
Christopher Manahan <chris.a.manahan@gmail.com>
2016-12-01 13:23:57 -0500

    current song reducer tests complete

---
+++ tests/utils/index.js
@@ -0,0 +1,9 @@
+export const sampleSong = {
+	artist: "dazzel-almond",
+	dj_name: "DJ Dull Stingray",
+	song_uri: "https://soundcloud.com/dazzel-almond/dark-ally",
+	time_priority: 0,
+	title: "Dark Ally",
+	uid: "iSyxOOxMjwau7rLEzUGx7zjjTz83",
+	vote_priority: 0
+}

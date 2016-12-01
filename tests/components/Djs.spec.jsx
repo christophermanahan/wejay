7a69d5c95db8f9c90daae6f6bb287d25a06c1ad5
Dillon Powers <dillon.c.powers@gmail.com>
2016-12-01 15:47:45 -0500

    comment out stuff

--- tests/components/Djs.spec.jsx
+++ tests/components/Djs.spec.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import { spy } from 'sinon';
+// import React from 'react';
+// import { expect } from 'chai';
+// import { shallow } from 'enzyme';
+// import { spy } from 'sinon';
 
-import DjsList from '../../app/components/DjsList';
+// import DjsList from '../../app/components/DjsList';
 
-describe('<Djs />', () => {
+// describe('<Djs />', () => {
 
-	console.log('Djs is', DjsList)
-	// let onTypeSpy, trackSearchSpy, search;
-	let djs;
+// 	console.log('Djs is', DjsList)
+// 	// let onTypeSpy, trackSearchSpy, search;
+// 	let djs;
 
-	beforeEach('Create shallow copy with two spy fns on props', () => {
-		// onTypeSpy = spy();
-		// trackSearchSpy = spy();
-		djs = shallow(<DjsList />);
-	});
+// 	beforeEach('Create shallow copy with two spy fns on props', () => {
+// 		// onTypeSpy = spy();
+// 		// trackSearchSpy = spy();
+// 		djs = shallow(<DjsList />);
+// 	});
 
-	it('should do some stuff', () => {
-		console.log('ran test');
-	});
+// 	it('should do some stuff', () => {
+// 		console.log('ran test');
+// 	});
+	
+// 	it('should do more stuff', () => {
+// 		console.log('ran test')
+// 	});
 
-	it('should do more stuff', () => {
-		console.log('ran test')
-	});
 
+// })
 
-})
-
-// // onType={ onTypeSpy } trackSearch={ trackSearchSpy }
+// // // onType={ onTypeSpy } trackSearch={ trackSearchSpy }

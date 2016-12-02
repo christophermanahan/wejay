660e5aeb660aaa9e6e71a8160c4f2bfb2a12d416
Shane Jarman <sjarman91@gmail.com>
2016-12-02 16:14:24 -0500

    set up tests

--- tests/firebase/firebossListeners.spec.jsx
+++ tests/firebase/firebossListeners.spec.jsx
@@ -34,17 +34,32 @@ const fireboss = new Fireboss(firebase, dispatchers, browserHistory);
 // create spy dispatchers
 // make sure they are called with the right data
 
-describe('---------- FIREBOSS LISTENER TESTS ----------', () => {
+describe('------ FIREBOSS LISTENER TESTS ------', () => {
+
+  let partiesRef = firebase.database().ref('parties')
+  let userPartiesRef = firebase.database().ref('user_parties')
+  let partyDjsRef = firebase.database().ref('party_djs')
+  let currentSongRef = firebase.database().ref('current_song')
+  let topTenRef = firebase.database().ref('top_ten')
+  let shadowQueueRef = firebase.database().ref('shadow_queue')
 
   describe('TESTING GLOBAL PARTIES LISTENER', () => {
     before('set up parties listener', done => {
-      // fireboss.createPartiesListener()
-      // firebase.addParty()
-      // set Timeout?
+      let hostId = "abc123";
+      fireboss.createPartiesListener()
+        .then(() => {
+          return partiesRef.set({[hostId]: sampleParty})
+        })
+        .then(() => {
+          setTimeout(() => done(), 500)
+        })
+        .catch(done)
     });
 
     after('turn off parties listener', done => {
-      // fireboss.database.ref('parties').off()
+      fireboss.database.ref('parties').off()
+        .then(() => done())
+        .catch(done)
     });
 
     it('parties dispatcher', () => {

54735da1b7abbee610774b21308120433becb8a5
Dillon Powers <dillon.c.powers@gmail.com>
2016-12-01 20:43:18 -0500

    finish pq but bugs

--- server/firechief.js
+++ server/firechief.js
@@ -13,7 +13,7 @@ class Firechief {
 	}
 
 	createPartyAddedListener() {
-		console.log("into createPartyAddedListener");
+		console.log('into createPartyAddedListener');
 		const partiesRef = this.db.ref('parties');
 		partiesRef.on('child_added', snapshot => {
 			const partyId = snapshot.val() && snapshot.val().id;
@@ -23,7 +23,7 @@ class Firechief {
 	}
 
 	createNewPartyListener(partyId) {
-		// console.log("into createNewPartyListener");
+		console.log('into createNewPartyListener');
 		const newPartyRef = this.db.ref('parties').child(partyId);
 
 		newPartyRef.on('value', snapshot => {
@@ -32,9 +32,9 @@ class Firechief {
 				// this.setCurrentSong(snapshot);
 
 				//need to spread off of the returned promise all array
-				console.log("into need song");
+				console.log('into need song');
 
-				this.masterReorder(partyId)
+				this.masterReorder(partyId);
 
 			}
 		});
@@ -43,59 +43,55 @@ class Firechief {
 	masterReorder(partyId) {
 		this.setCurrentSong(partyId)
 		.then(() => {
-			return this.pullFromShadowQueue(partyId)
+			return this.pullFromShadowQueue(partyId);
 		})
 		.then(resultsArr => {
-			if(!resultsArr) return;
-			const uid = resultsArr[0]
+			if (!resultsArr) return;
+			const uid = resultsArr[0];
 			return this.pullFromPersonalQueue(partyId, uid);
 		})
-		.catch(console.error)
+		.then(() => {
+			console.log('finished a full re-order, including PQ!');
+		})
+		.catch(console.error);
 	}
 
 
 	pullFromShadowQueue(partyId){
-		return this.getHighestPriority(partyId, "shadow_queue")		//get hi pri from SQ
+		return this.getHighestPriority(partyId, 'shadow_queue')		//get hi pri from SQ
 		.then(song => {
-			if(!song) return;
-			const songId = this.deconstructSongObject(song, "hash")
+			if (!song) return;
+			const songId = this.deconstructSongObject(song, 'hash');
 			const songObj = this.deconstructSongObject(song);
 			const { uid } = songObj;
 			Promise.all([
 				this.promisifySQuid(uid),
-				this.db.ref("top_ten").child(partyId).update(song), 	// put onto top ten
-				this.removeSong(partyId, "shadow_queue", songId) 			//delete song from SQ
+				this.db.ref('top_ten').child(partyId).update(song), 	// put onto top ten
+				this.removeSong(partyId, 'shadow_queue', songId) 			//delete song from SQ
 			])
-			.catch(console.error)
-		})
+			.catch(console.error);
+		});
 	}
 
 	promisifySQuid(uid){
 		return new Promise((resolve, reject) => {
 			resolve(uid);
-		})
+		});
 	}
 
 	pullFromPersonalQueue(partyId, uid) {
 		//grab song from PQ
-
-		this.getHighestPriority(partyId, "personal_queue", uid)
+		return this.getHighestPriority(partyId, 'personal_queue', uid)
 		.then(song => {
-			if(!song) return
+			if (!song) return;
 			//push onto SQ
-			const songId = this.deconstructSongObject(song, "hash")
-			// Promise.all([])
-
-
-
-
+			const songId = this.deconstructSongObject(song, 'hash');
+			return Promise.all([
+				this.db.ref('shadow_queue').child(partyId).update(song), // put onto shadow queue
+				this.removeSong(partyId, 'personal_queue', songId)			 // delete song from PQ
+			]);
 		})
-
-
-
-
-		//delete from PQ
-
+		.catch(console.error);
 	}
 
 	checkIfNeedSong(snapshot) {
@@ -104,7 +100,7 @@ class Firechief {
 
 	deconstructSongObject(song, option) {						//in Firebase unique hash vals point to songs
 		const hash = Object.keys(song)[0]							//sometimes we want the key hash val,
-		return option === "hash" ? hash : song[hash]	//sometimes the song object that it points to
+		return option === 'hash' ? hash : song[hash]	//sometimes the song object that it points to
 	}
 
 	setNeedSongToFalse(partyId) {
@@ -113,15 +109,15 @@ class Firechief {
 
 	setCurrentSong(partyId) {
 		const currentSongRef = this.db.ref('current_song').child(partyId)
-		console.log("into setCurrentSong");
+		console.log('into setCurrentSong');
 
-		return this.getHighestPriority(partyId, "top_ten")
+		return this.getHighestPriority(partyId, 'top_ten')
 		.then(song => {		//song ==> {nextSongId: nextSong}
-			if(!song) {
+			if (!song) {
 				return this.setNeedSongToFalse(partyId)
 			}
 			const newSong = this.deconstructSongObject(song)
-			const newSongId = this.deconstructSongObject(song, "hash")
+			const newSongId = this.deconstructSongObject(song, 'hash')
 
 			return Promise.all([
 				//set current song to song
@@ -129,18 +125,23 @@ class Firechief {
 				// update needSong to false
 				this.setNeedSongToFalse(partyId),
 				// remove song from top ten
-				this.removeSong(partyId, "top_ten", newSongId)
-			])
+				this.removeSong(partyId, 'top_ten', newSongId)
+			]);
 		})
 		.catch(console.error);
 	}
 
 
-	removeSong(partyId, queue, hashId) {
-		return this.db.ref(queue).child(partyId).child(hashId).remove()
+	removeSong(partyId, queue, hashId, uid) {
+		const queueRef = (uid) ?
+			this.db.ref('party_djs').child(partyId).child(uid).child(queue)
+			:
+			this.db.ref(queue).child(partyId);
+
+		return queueRef.child(hashId).remove()
 		.then(err => {
-			if(err){
-				throw new Error(err)
+			if (err){
+				throw new Error(err);
 			} else {
 				return true;
 			}
@@ -153,13 +154,13 @@ class Firechief {
 		const queueRef = (uid) ?
 			this.db.ref('party_djs').child(partyId).child(uid).child(queue)
 			:
-			this.db.ref(queue).child(partyId)
+			this.db.ref(queue).child(partyId);
 
 		return queueRef.once('value')
 		.then(snapshot => {
 			const songsInQueue = snapshot && snapshot.val();
 
-			if(!songsInQueue){
+			if (!songsInQueue){
 				return
 			}
 
@@ -167,25 +168,20 @@ class Firechief {
 			let nextSongPriority = -1000;		//needs to address --> collisions when priority scores are equal
 			let nextSongId;
 
-			for (let song in songsInQueue){
+			for (let song in songsInQueue) {
 
-				let netPriority = songsInQueue[song].time_priority + songsInQueue[song].vote_priority
+				let netPriority = songsInQueue[song].time_priority + songsInQueue[song].vote_priority;
 
-				if(netPriority > nextSongPriority){
-					nextSong = songsInQueue[song]
+				if (netPriority > nextSongPriority){
+					nextSong = songsInQueue[song];
 					nextSongPriority = netPriority;
-					nextSongId = song
+					nextSongId = song;
 				}
 			}
-			return {[nextSongId]: nextSong}
+			return {[nextSongId]: nextSong};
 		})
 		.catch(console.error);
 	}
-
-
-
-
-
 
 }
 

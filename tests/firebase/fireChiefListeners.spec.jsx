026914ccf133936051a8a9a3977d5d1048e72450
Matthew Elmquist <mattelmquist@gmail.com>
2016-12-02 20:37:58 -0500

    progress

--- tests/firebase/fireChiefListeners.spec.jsx
+++ tests/firebase/fireChiefListeners.spec.jsx
@@ -22,33 +22,42 @@ describe('---------- FIRECHIEF LISTENER TESTS ----------', () => {
 
     let partiesRef = db.ref('parties');
 
-    let spyCreatePartyAddedListener;
+    let spyCreateNewPartyListener;
 
-    before('Create spy that wraps createPartyAddedListener', done => {
-      spyCreatePartyAddedListener = spy(firechief, "createPartyAddedListener");
-      spyCreatePartyAddedListener()
-      partiesRef.set({[idForNewSampleParty]: sampleNewParty})
+    before('Create spy that wraps createNewPartyListener', done => {
+      spyCreateNewPartyListener = spy(firechief, "createNewPartyListener");
+      firechief.createPartyAddedListener()
+      return partiesRef.child(idForNewSampleParty).set(sampleNewParty)
+      // partiesRef.push(sampleNewParty)
+      // partiesRef.set({[idForNewSampleParty]: sampleNewParty})
       .then(() => {
-        done()
+        return new Promise((res, rej) => {
+          res(setTimeout(() => {}, 500))
+        })
       })
-      .catch(done);
-
+    .then(() => {
+      done();
     })
+    .catch(done);
 
-    after('clear spy', () => {
-      spyCreatePartyAddedListener.restore();
-    })
+    // after('clear spy', () => {
+    //   spyCreateNewPartyListener.restore();
+    //   firechief.removePartyListener(idForNewSampleParty);
+    // })
 
-    // it('should have been called with createNewPartyListener')
 
   })
 
+  it('should call createNewPartyListener when party is added', () => {
+    console.log("CALL COUNT HEREHEHREHREHREHHRE", spyCreateNewPartyListener.callCount);
+    // expect(spyCreateNewPartyListener.called).to.be.true;
+    console.log("ASD");
+  })
 
+  // it('should call createNewPartyListener when party is added', () => {
+  //   expect(spyCreateNewPartyListener.calledOnce).to.equal(true);
+  // })
 
-
-
-
-
-
+  });
 
 });

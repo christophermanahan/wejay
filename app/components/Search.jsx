87f28887a20bf1b73353bad97bb7d521bab24e64
Dillon Powers <dillon.c.powers@gmail.com>
2016-12-04 20:46:12 -0500

    fiddle around

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -19,29 +19,29 @@ const RaisedButtonStyle = {width: '20%', marginLeft: '20px'};
 /* -----------------    DUMB COMPONENT     ------------------ */
 
 const DumbSearch = props => {
-  const { onType, trackSearch, onClear, clearedVal } = props;
+  const { onType, trackSearch, onClear, query } = props;
   return (
       <Row>
         <Col xs={12}>
           <form onSubmit={ trackSearch }>
             <Row>
               <Col xs={12}>
-                <TextField
-                  style={TextFieldStyle}
-                  onChange={ onType }
-                  floatingLabelText="Search By Track"
-                  value={clearedVal}
-                />
+                  <TextField
+                    style={TextFieldStyle}
+                    onChange={ onType }
+                    value={ query }
+                    floatingLabelText="Search By Track"
+                  />
 
-                <IconButton onTouchTap={onClear}>
-                  <i className="material-icons">highlight_off</i>
-                </IconButton>
+                  <IconButton onTouchTap={onClear}>
+                    <i className="material-icons">highlight_off</i>
+                  </IconButton>
 
-                <RaisedButton
-                  label="Search"
-                  onTouchTap={ trackSearch }
-                  style={RaisedButtonStyle}
-                />
+                  <RaisedButton
+                    label="Search"
+                    type="submit"
+                    style={RaisedButtonStyle}
+                  />
               </Col>
             </Row>
            </form>
@@ -53,98 +53,101 @@ const DumbSearch = props => {
 
 /* -----------------    STATEFUL REACT COMPONENT     ------------------ */
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      query: '',
-      snackbarOpen: false,
-      songDestination: '',
-      clearedVal: ''
-    };
+  class Search extends Component {
+    constructor(props) {
+      super(props);
 
-    this.onType = this.onType.bind(this);
-    this.onClear = this.onClear.bind(this);
-    this.trackSearch = this.trackSearch.bind(this);
-    this.addToQueue = this.addToQueue.bind(this);
-    this.openSnackbar = this.openSnackbar.bind(this);
-    this.closeSnackbar = this.closeSnackbar.bind(this);
+      this.state = {
+        query: '',
+        snackbarOpen: false,
+        songDestination: ''
+      };
 
+      this.onType = this.onType.bind(this);
+      this.onClear = this.onClear.bind(this);
+      this.trackSearch = this.trackSearch.bind(this);
+      this.addToQueue = this.addToQueue.bind(this);
+      this.openSnackbar = this.openSnackbar.bind(this);
+      this.closeSnackbar = this.closeSnackbar.bind(this);
+
+    }
+
+    onClear(evt) {
+      evt.preventDefault();
+      this.setState({ query: '' })
+      console.log('STATE AFTER CLEAR:', this.state.query)
+    }
+
+    onType(evt) {
+      evt.preventDefault();
+      let query = evt.target.value;
+      console.log('QUERY IS', query)
+      this.setState({ query });
+      console.log('STATE is now:', this.state.query)
+    }
+
+    openSnackbar(songDestination) {
+      this.setState({snackbarOpen: true, songDestination});
+    }
+
+    closeSnackbar() {
+      this.setState({snackbarOpen: false, songDestination: ''})
+    }
+
+    trackSearch(evt){
+      evt.preventDefault();
+      const {tracksearch} = this.props;
+      const {query} = this.state;
+      if (!query.length) return;
+      tracksearch(query);
+    }
+
+    addToQueue(song_uri, title, artist, artwork_url, duration) {
+      const { user, currentParty, fireboss } = this.props;
+      const partyId = currentParty.id;
+      const { uid } = user;
+      const { dj_name } = this.props.djs[uid];
+      const song = { song_uri, title, artist, artwork_url, dj_name, time_priority: 0, vote_priority: 0, duration};
+
+      fireboss.submitUserSong(partyId, user, song, this.openSnackbar)
+    }
+
+    render() {
+      const { searchResults, clearedVal } = this.props;
+      return (
+          <div>
+            <Row>
+              <Col xs={10} xsOffset={1}>
+                <DumbSearch
+                  clearedVal={clearedVal}
+                  onClear= { this.onClear }
+                  onType={ this.onType }
+                  trackSearch={ this.trackSearch }
+                  linkWithState={this.linkWithState}
+                />
+              </Col>
+              <Col xs={8} xsOffset={2}>
+                <Snackbar
+                  open={this.state.snackbarOpen}
+                  message={this.state.songDestination}
+                  autoHideDuration={4000}
+                  onRequestClose={this.closeSnackbar}
+                  contentStyle={{ fontSize: '1.2em' }}
+                  bodyStyle={{ height: '2.5em', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                />
+              </Col>
+              <Col xs={12}>
+                <SearchResults
+                  searchResults={ searchResults }
+                  addToQueue={ this.addToQueue }
+                />
+              </Col>
+            </Row>
+          </div>
+      );
+    }
   }
-
-  onClear(){
-    console.log("CLICKED");
-    this.setState({ clearedVal: '' })
-  }
-
-  onType(evt) {
-    evt.preventDefault();
-    let query = evt.target.value;
-    this.setState({ query });
-  }
-
-  openSnackbar(songDestination) {
-    this.setState({snackbarOpen: true, songDestination});
-  }
-
-  closeSnackbar() {
-    this.setState({snackbarOpen: false, songDestination: ''})
-  }
-
-
-  trackSearch(evt){
-    evt.preventDefault();
-    const {tracksearch} = this.props;
-    const {query} = this.state;
-    if (!query.length) return;
-    tracksearch(query);
-  }
-
-  addToQueue(song_uri, title, artist, artwork_url, duration) {
-    const { user, currentParty, fireboss } = this.props;
-    const partyId = currentParty.id;
-    const { uid } = user;
-    const { dj_name } = this.props.djs[uid];
-    const song = { song_uri, title, artist, artwork_url, dj_name, time_priority: 0, vote_priority: 0, duration};
-
-    fireboss.submitUserSong(partyId, user, song, this.openSnackbar)
-  }
-
-  render() {
-    const { searchResults, clearedVal } = this.props;
-    return (
-        <div>
-          <Row>
-            <Col xs={10} xsOffset={1}>
-              <DumbSearch
-                clearedVal={clearedVal}
-                onClear= { this.onClear }
-                onType={ this.onType }
-                trackSearch={ this.trackSearch }
-              />
-            </Col>
-            <Col xs={8} xsOffset={2}>
-              <Snackbar
-                open={this.state.snackbarOpen}
-                message={this.state.songDestination}
-                autoHideDuration={4000}
-                onRequestClose={this.closeSnackbar}
-                contentStyle={{ fontSize: '1.2em' }}
-                bodyStyle={{ height: '2.5em', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-              />
-            </Col>
-            <Col xs={12}>
-              <SearchResults
-                searchResults={ searchResults }
-                addToQueue={ this.addToQueue }
-              />
-            </Col>
-          </Row>
-        </div>
-    );
-  }
-}
 
 
 /* -----------------    CONTAINER     ------------------ */
